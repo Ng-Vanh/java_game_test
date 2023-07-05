3cf823e65f0a665473bd7a5bb326e42d06f20129
Ng-Vanh <nguyenvietanh2234@gmail.com>
2023-07-06 01:32:34 +0700

    Test Animation

--- src/UI/GamePanel.java
+++ src/UI/GamePanel.java
@@ -1,5 +1,8 @@
 package UI;
 
+import Effect.Animation;
+import Effect.FrameImage;
+
 import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
@@ -14,18 +17,34 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     private boolean isRunning;
     private InputManage inputManage;
 
-    BufferedImage image;
-    BufferedImage subImage;
-
+    /* Test animation */
+    FrameImage frame1, frame2, frame3;
+    Animation anim;
+    /*----------------------------------*/
     public GamePanel()
     {
         inputManage = new InputManage();
+        /*TestAnimation*/
         try {
-            image = ImageIO.read(new File("src/img/Textures/classic.png"));
-            subImage = image.getSubimage(0,0,16,48);
+            BufferedImage image = ImageIO.read(new File("src/img/Textures/classic.png"));
+            BufferedImage image1 = image.getSubimage(16, 0,16, 16);
+            frame1 = new FrameImage("frame1", image1);
+
+            BufferedImage image2 = image.getSubimage(16,16,16,16);
+            BufferedImage image3 = image.getSubimage(16,32,16,16);
+            frame2 = new FrameImage("frame2", image2);
+            frame3 = new FrameImage("frame3", image3);
+
+            anim = new Animation();
+            anim.add(frame1,150*1000000);
+            anim.add(frame2,150*1000000);
+            anim.add(frame3,150*1000000);
+
         } catch (IOException e) {
-            e.printStackTrace();
+            throw new RuntimeException(e);
         }
+/*------------------------------------------------------------*/
+
     }
 
     @Override
@@ -33,6 +52,10 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
     {
         g.setColor(Color.CYAN);
         g.fillRect(0,0,GameFrame.SCREEN_WIDTH, GameFrame.SCREEN_HEIGHT);
+
+        Graphics2D g2 = (Graphics2D) g;
+        anim.Update(System.nanoTime());
+        anim.draw( 100,100, g2);
 
     }
     public void startGame()
@@ -55,6 +78,8 @@ public class GamePanel extends JPanel implements Runnable, KeyListener {
         beginTime = System.nanoTime();
         while (isRunning)
         {
+
+            repaint();
             long deltaTime = System.nanoTime() - beginTime;
             sleepTime = timeOneFrame - deltaTime;
 
